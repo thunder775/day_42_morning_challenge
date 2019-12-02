aa192bface7aa640e145836436ce3ecab14287ce
McLarenCollege <hello@mclarencollege.in>
2019-12-02 08:29:33 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,14 @@
+// Write a function that takes an array of unordered integers and logs percentile
+// values at 25%, 50%, 75%, 90%, 95%, 99% and 100%
+
+// For example, given the list [8, 6, 6, 20, 9, 1, 12, 16, 3, 16, 22, 2] it should log the following:
+//  25% of the numbers are less than or equal to 3
+//  50% of the numbers are less than or equal to 8
+//  75% of the numbers are less than or equal to 16
+//  90% of the numbers are less than or equal to 20
+//  95% of the numbers are less than or equal to 22
+//  99% of the numbers are less than or equal to 22
+//  100% of the numbers are less than or equal to 22
+
+main() {
+}
