45f9c3e0511a9011edf0791442c7eb268b83de6b
thunder775 <rahulmasih61@gmail.com>
2019-12-02 10:50:07 +0530

    done

--- bin/main.dart
+++ bin/main.dart
@@ -9,6 +9,30 @@
 //  95% of the numbers are less than or equal to 22
 //  99% of the numbers are less than or equal to 22
 //  100% of the numbers are less than or equal to 22
-
+List<List<int>> log(List<int> input) {
+  List<int> percentages = [25, 50, 75, 90, 95, 99, 100];
+  input.sort();
+  List<List<int>> output = [];
+  percentages.forEach((percentValue) {
+    output.add(calculateNPercentOfIntegers(percentValue, input));
+  });
+//  print(output);
+  return output;
+}
+List<int> calculateNPercentOfIntegers(int percent, List input) {
+  List<int> temp = [];
+  int numberOfIntegers = (input.length * (percent / 100)).ceil();
+  temp.add(percent);
+  temp.add(input[numberOfIntegers - 1]);
+  return temp;
+}
 main() {
+
+  log([1,2,3,4]);
+   log([8, 6, 6, 20, 9, 1, 12, 16, 3, 16, 22, 2]);
+
+//  for(var x in log([8, 6, 6, 20, 9, 1, 12, 16, 3, 16, 22, 2])){
+//    print('${x[0]}% of the numbers are less than or equal to ${x[1]}');
+//  }
+
 }
--- test/Day_42_Morning_Challenge_test.dart
+++ test/Day_42_Morning_Challenge_test.dart
@@ -1,6 +1,11 @@
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
-  test('Test Case 1', () {
+  test('Test Case 1', () {});
+  test('percentile log', () {
+    expect( log([1, 2, 3, 4]), [[25, 1], [50, 2], [75, 3], [90, 4], [95, 4], [99, 4], [100, 4]]);
+    expect(
+        log([8, 6, 6, 20, 9, 1, 12, 16, 3, 16, 22, 2]), [[25, 3], [50, 8], [75, 16], [90, 20], [95, 22], [99, 22], [100, 22]]);
   });
 }
